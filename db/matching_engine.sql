f2f26f22d914161dbb770991fac5c68b478b5999
David Milet <david.milet@gmail.com>
2016-10-12 15:58:42 +0000

    matching engine algorithm

---
+++ db/matching_engine.sql
@@ -0,0 +1,192 @@
+﻿DO
+$$
+DECLARE
+
+RADIUS_MAX_ALLOWED integer := 100;
+
+drive_offer_row dev.drive_offer%ROWTYPE;
+ride_request_row dev.ride_request%ROWTYPE;
+cnt integer;
+match_points integer;
+time_criteria_points integer;
+
+ride_times_rider text[];
+ride_times_driver text[];
+driver_time text;
+rider_time text;
+seconds_diff integer;
+
+start_ride_time timestamp with time zone;
+end_ride_time timestamp with time zone;
+start_drive_time timestamp with time zone;
+end_drive_time timestamp with time zone;
+
+zip_origin nov2016.zip_codes%ROWTYPE;  -- Driver's origin
+zip_pickup nov2016.zip_codes%ROWTYPE;  -- Rider's pickup
+zip_dropoff nov2016.zip_codes%ROWTYPE; -- Rider's dropoff
+
+distance_origin_pickup double precision;  -- From driver origin to rider pickup point
+distance_origin_dropoff double precision; -- From driver origin to rider drop off point
+
+BEGIN
+
+    cnt := 0;
+    FOR ride_request_row in SELECT * from dev.ride_request r
+        WHERE r.state='Pending'
+        AND length(r."AvailableRideTimesJSON") > 0
+    LOOP
+    
+        -- DriverCollectionZIP
+        -- DriverCollectionRadius
+        
+        SELECT * INTO zip_pickup FROM nov2016.zip_codes WHERE zip=ride_request_row."RiderCollectionZIP";
+        SELECT * INTO zip_dropoff FROM nov2016.zip_codes WHERE zip=ride_request_row."RiderDropOffZIP";
+        
+        -- split AvailableRideTimesJSON in individual time intervals
+        ride_times_rider := string_to_array(ride_request_row."AvailableRideTimesJSON", '|');
+        
+        FOR drive_offer_row in SELECT * from dev.drive_offer d
+            WHERE state='Pending'
+            AND ((ride_request_row."NeedWheelchair"=true AND d."DriverCanLoadRiderWithWheelchair" = true) -- driver must be able to transport wheelchair if rider needs it
+                OR ride_request_row."NeedWheelchair"=false)   -- but a driver equipped for wheelchair may drive someone who does not need one
+            AND ride_request_row."TotalPartySize" <= d."SeatCount"  -- driver must be able to accommodate the entire party in one ride
+            AND length(d."AvailableDriveTimesJSON") > 0
+        LOOP
+        
+            match_points := 0;
+            
+            -- split AvailableRideTimesJSON in individual time intervals
+            -- NOTE : we do not want actual JSON here...
+            -- FORMAT should be like this 
+            -- 2016-10-01T08:00:00-0500/2016-10-01T10:00:00-0500|2016-10-01T10:00:00-0500/2016-10-01T22:00:00-0500|2016-10-01T22:00:00-0500/2016-10-01T23:00:00-0500
+            ride_times_driver := string_to_array(drive_offer_row."AvailableDriveTimesJSON", '|');
+            
+            SELECT * INTO zip_origin FROM nov2016.zip_codes WHERE zip=drive_offer_row."DriverCollectionZIP";
+            
+            -- Compare RiderCollectionZIP with DriverCollectionZIP / DriverCollectionRadius
+            distance_origin_pickup := nov2016.distance(
+                        zip_origin.latitude_numeric,
+                        zip_origin.longitude_numeric,
+                        zip_pickup.latitude_numeric,
+                        zip_pickup.longitude_numeric);
+            
+            distance_origin_dropoff := nov2016.distance(
+                        zip_origin.latitude_numeric,
+                        zip_origin.longitude_numeric,
+                        zip_dropoff.latitude_numeric,
+                        zip_dropoff.longitude_numeric);
+
+            IF distance_origin_pickup < RADIUS_MAX_ALLOWED AND distance_origin_dropoff < RADIUS_MAX_ALLOWED
+            THEN
+
+                -- driver/rider distance ranking
+                IF distance_origin_pickup < drive_offer_row."DriverCollectionRadius" 
+                    AND distance_origin_dropoff < drive_offer_row."DriverCollectionRadius"
+                THEN
+                    match_points := match_points + 100;   -- 100 point if the radius criteria is met
+                    match_points := match_points + RADIUS_MAX_ALLOWED - distance_origin_pickup; -- closest distance gets more points
+                END IF; 
+                
+                -- vulnerable rider matching
+                IF ride_request_row."RiderIsVulnerable" = false
+                THEN
+                    match_points := match_points + 200;
+                ELSIF ride_request_row."RiderIsVulnerable" = true 
+                    AND drive_offer_row."DrivingOnBehalfOfOrganization" 
+                THEN
+                    match_points := match_points + 200;
+                END IF;
+                
+                -- time matching
+                -- Each combination of rider time and driver time can give a potential match
+                FOREACH driver_time IN ARRAY ride_times_driver
+                LOOP
+                    FOREACH rider_time IN ARRAY ride_times_rider
+                    LOOP
+                        
+                        -- each time interval is in ISO8601 format
+                        -- 2016-10-23T10:00:00-0500/2016-10-23T11:00:00-0500
+                        start_ride_time := substr(rider_time, 1, 24)::timestamp with time zone;
+                        end_ride_time := substr(rider_time, 26, 24)::timestamp with time zone;
+                        start_drive_time := substr(driver_time, 1, 24)::timestamp with time zone;
+                        end_drive_time := substr(driver_time, 26, 24)::timestamp with time zone;
+                        
+                        time_criteria_points := 200;
+                        
+                        IF end_drive_time < start_ride_time       -- [ddddd]  [rrrrrr]
+                            OR end_ride_time < start_drive_time   -- [rrrrr]  [dddddd]
+                        THEN
+                            -- we're totally disconnected
+                            
+                            IF end_drive_time < start_ride_time
+                            THEN
+                            
+                                -- substracts one point per minute the driver is outside the rider interval
+                                time_criteria_points := 
+                                    time_criteria_points - abs(EXTRACT(EPOCH FROM (start_ride_time - end_drive_time))) / 60;
+                            ELSIF end_ride_time < start_drive_time
+                            THEN
+                                time_criteria_points := 
+                                    time_criteria_points - abs(EXTRACT(EPOCH FROM (start_drive_time - end_ride_time))) / 60;
+                            END IF;
+                            
+                            if time_criteria_points < 0
+                            THEN
+                                time_criteria_points := 0; 
+                            END IF;
+                            
+                        ELSIF start_drive_time < start_ride_time  -- [ddd[rdrdrdrdrd]ddd] 
+                            AND end_drive_time > end_ride_time
+                        THEN
+                            -- perfect! we're in the interval
+                        ELSIF start_drive_time < start_ride_time  -- [ddddddd[rdrdrd]rrrr]
+                            AND start_ride_time < end_drive_time
+                        THEN
+                            -- We're at least partially in the interval
+                        ELSIF  start_ride_time < start_drive_time -- [rrrrr[rdrdrd]ddddd]
+                            AND start_drive_time < end_ride_time
+                        THEN
+                            -- We're at least partially in the interval
+                        ELSIF start_ride_time < start_drive_time  -- [rrr[rdrdrdrdrd]rrrrr]
+                            AND end_drive_time < end_ride_time
+                        THEN
+                            -- We're completely in the interval
+                        END IF;
+                        
+                        
+                        IF match_points + time_criteria_points >= 300
+                        THEN
+                        
+                        RAISE NOTICE '% %, DT=%, RT=% SCORE=%', 
+                            drive_offer_row."DriverLastName", 
+                            ride_request_row."RiderLastName", 
+                            driver_time,
+                            rider_time,
+                            match_points + time_criteria_points;
+                        END IF;
+                        cnt := cnt +1;
+                        
+                    END LOOP;
+                    
+                    
+                END LOOP;
+
+            END IF;
+          
+
+          
+            
+            
+            -- We retain only matches with 300 points or more
+            --IF match_points >= 300
+            --THEN
+            
+            --END IF;
+        END LOOP;
+    
+    END LOOP;
+
+RAISE NOTICE 'Count=%', cnt;
+
+END;
+$$
--- db/test/populate_test_data.py
+++ db/test/populate_test_data.py
@@ -1,6 +1,7 @@
 from urllib.parse import urlencode
 from urllib.request import Request, urlopen
 import csv
+import json
 
 # This will HTTP POST to the node.js app
 
@@ -42,11 +43,13 @@ with open('carpool_test_databank_driver.csv', newline='\n') as csvfile:
                 driverValues['DriverCanLoadRiderWithWheelchair']=True
             else:
                 driverValues['DriverCanLoadRiderWithWheelchair']=False
-            ride_times = row[5].split('|')
-            driverValues['AvailableDriveTimesJSON'] = []
-            for a_time in ride_times:
-                driverValues['AvailableDriveTimesJSON'].append(a_time)
+            driverValues['AvailableDriveTimesJSON'] = row[5]
+            #ride_times = row[5].split('|')
+            #times_arr = []
+            #for a_time in ride_times:
+            #    times_arr.append(a_time)
 
+            #driverValues['AvailableDriveTimesJSON'] = json.dumps(times_arr)
             data = urlencode(driverValues).encode("utf-8")
             req = Request(url, data)
             response = urlopen(req)
@@ -91,11 +94,12 @@ with open('carpool_test_databank_rider.csv', newline='\n') as csvfile:
                 riderValues['NeedWheelchair']=True
             else:
                 riderValues['NeedWheelchair']=False
-            ride_times = row[5].split('|')
-            riderValues['AvailableRideTimesJSON'] = []
-            for a_time in ride_times:
-                riderValues['AvailableRideTimesJSON'].append(a_time)
-
+            #ride_times = row[5].split('|')
+            #times_arr = []
+            #for a_time in ride_times:
+            #    times_arr.append(a_time)
+            #riderValues['AvailableRideTimesJSON'] = json.dumps(times_arr)
+            riderValues['AvailableRideTimesJSON'] = row[5]
             data = urlencode(riderValues).encode("utf-8")
             req = Request(url, data)
             response = urlopen(req)
