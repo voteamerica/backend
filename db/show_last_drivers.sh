97b468b6aaec9557bcbe739e72e39a549c82c9c4
David Milet <david.milet@gmail.com>
2016-10-15 10:48:59 +0000

    New supportability commands, new schema objects

---
+++ db/show_last_drivers.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# USAGE : $0 [dbname] [max-rows]
+
+PGDATABASE="carpool"
+if [[ "X$1" != "X" ]]
+then
+PGDATABASE=$1
+fi
+
+LIMIT="LIMIT 25"
+if [[ "X$2" != "X" ]]
+then
+LIMIT="LIMIT $2"
+fi
+
+echo $PGDATABASE $LIMIT
+
+psql -h /tmp $PGDATABASE <<RPT
+select * from stage.vw_drive_offer order by last_updated_ts desc $LIMIT
+RPT
+
+
