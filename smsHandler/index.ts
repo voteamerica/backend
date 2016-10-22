790ea5e768d9ab54565032175abe2ff7d3cb86a7
Jon Kelly <jonkelly@jmkassocs.com>
2016-10-22 17:26:24 +0100

    added table names, misc

--- smsHandler/index.ts
+++ smsHandler/index.ts
@@ -28,7 +28,7 @@ var DELAY = process.env.CP_DELAY || 10000;
 
 var Pool = require('pg').Pool;
 var pool = new Pool({
-    idleTimeoutMillis: 2000 //close idle clients after 1 second
+  idleTimeoutMillis: 2000 //close idle clients after 1 second
 });
 
 // var SCHEMA_NAME = 'stage';
@@ -36,71 +36,96 @@ var SCHEMA_NAME = 'nov2016';
 // var SWEEPER_RIDERS_FUNCTION = 'create_riders()';
 // var SWEEPER_DRIVERS_FUNCTION = 'create_drivers()';
 // random thing to call
-const UNMATCHED_DRIVERS_VIEW    = 'vw_unmatched_riders';
-const MATCH_TABLE    = 'match';
+// const UNMATCHED_DRIVERS_VIEW  = 'vw_unmatched_riders';
+// const MATCH_TABLE             = 'match';
 
+const OUTGOING_EMAIL_TABLE    = 'outgoing_email';
+const OUTGOING_SMS_TABLE      = 'outgoing_sms';
 
 var currentFunction = 0;
 
 setInterval(function () {
-    dbGetData(pool, [dbExecuteRidersFunctionString
-        // ,
-        // dbExecuteDriversFunctionString
-    ]);
+  dbGetData(pool, [dbGetOutgoingEmailString
+      // ,
+      // dbExecuteDriversFunctionString
+  ]);
 }, DELAY);
 
-function dbExecuteRidersFunctionString() {
-    // return 'SELECT * FROM ' + SCHEMA_NAME + '.' + UNMATCHED_DRIVERS_VIEW
-    // return 'SELECT * FROM '
-    return 'SELECT uuid_driver FROM '
-     + SCHEMA_NAME + '.' + 
-    // UNMATCHED_DRIVERS_VIEW
-    MATCH_TABLE 
-        //SWEEPER_RIDERS_FUNCTION
-        ;
+function dbGetOutgoingEmailString() {
+  // return 'SELECT * FROM ' + SCHEMA_NAME + '.' + UNMATCHED_DRIVERS_VIEW
+  // return 'SELECT * FROM '
+  return 'SELECT uuid_driver FROM '
+          + SCHEMA_NAME + '.' 
+          + OUTGOING_EMAIL_TABLE
+  // UNMATCHED_DRIVERS_VIEW
+  // MATCH_TABLE 
+      //SWEEPER_RIDERS_FUNCTION
+      ;
 }
 
-// function dbExecuteDriversFunctionString() {
-//     return 'select ' + SCHEMA_NAME + '.' + SWEEPER_DRIVERS_FUNCTION;
-// }
+function dbGetOutgoingSmsString() {
+  return 'SELECT uuid_driver FROM '
+          + SCHEMA_NAME + '.' 
+          + OUTGOING_EMAIL_TABLE
+      ;
+}
 
 function dbGetData(pool, executeFunctionArray) {
-    var fnExecuteFunction = executeFunctionArray[currentFunction++];
-    if (currentFunction >= executeFunctionArray.length) {
-        currentFunction = 0;
+  var fnExecuteFunction = executeFunctionArray[currentFunction++];
+  if (currentFunction >= executeFunctionArray.length) {
+      currentFunction = 0;
+  }
+  
+  console.log("array len : " + executeFunctionArray.length);
+  console.log("fn index: " + currentFunction);
+  
+  var queryString = fnExecuteFunction();
+  
+  console.log("queryString: " + queryString);
+
+  pool
+  .query(queryString)
+  .then(function (result) {
+    var firstRowAsString = "";
+
+    if (result !== undefined && result.rows !== undefined) {
+      // result.rows.forEach( val => console.log(val));
+      result.rows.forEach(function (val) { 
+        console.log("select: " + JSON.stringify(val)); 
+      });
+
+      firstRowAsString = JSON.stringify(result.rows[0]);
+
+      var uuid_driver = result.rows[0].uuid_driver.toString();
+
+      var state   = result.rows[0].state.toString();
+      var msgText = result.rows[0].body.toString();
+
+  //      id serial NOT NULL,
+  // created_ts timestamp without time zone NOT NULL DEFAULT timezone('utc'::text, now()),
+  // last_updated_ts timestamp without time zone NOT NULL DEFAULT timezone('utc'::text, now()),
+  // state character varying(30) NOT NULL DEFAULT 'Pending'::character varying,
+  // recipient character varying(255) NOT NULL,
+  // subject character varying(255) NOT NULL,
+  // body text NOT NULL,
+  // emission_info text,
+
+
+
+      console.log("uuid: ", uuid_driver);
+      makeCalls(uuid_driver);
     }
-    
-    console.log("array len : " + executeFunctionArray.length);
-    console.log("fn index: " + currentFunction);
-    
-    var queryString = fnExecuteFunction();
-    
-    console.log("queryString: " + queryString);
 
-    pool.query(queryString)
-        .then(function (result) {
-        var firstRowAsString = "";
-        if (result !== undefined && result.rows !== undefined) {
-            // result.rows.forEach( val => console.log(val));
-            result.rows.forEach(function (val) { return console.log("select: " + JSON.stringify(val)); });
-            firstRowAsString = JSON.stringify(result.rows[0]);
+    console.error("executed query: " + firstRowAsString);
+  })
+  .catch(function (e) {
+    var message = e.message || '';
+    var stack = e.stack || '';
 
-var uuid_driver = result.rows[0].uuid_driver.toString();
-console.log("uuid: ", uuid_driver);
-makeCalls(uuid_driver);
-
-        }
-        console.error("executed query: " + firstRowAsString);
-        // reply(results.success + firstRowAsString);
-    })
-        .catch(function (e) {
-        var message = e.message || '';
-        var stack = e.stack || '';
-        console.error(
-        // results.failure, 
-        message, stack);
-        // reply(results.failure + message).code(500);
-    });
+    console.error(
+      // results.failure, 
+      message, stack);
+  });
 }
 
 
