2805b9be49907975cb61a25fde293e16bde917ad
David Milet <david.milet@gmail.com>
2017-04-11 14:52:37 +0000

    new schema name and use python2

--- db/start_matching_engine.sh
+++ db/start_matching_engine.sh
@@ -13,6 +13,6 @@ while true
 do
 date  >> matching_engine_$1.log
 psql -h /tmp $1 < matching_engine.sql 2>&1 >> matching_engine_$1.log
-python3.5 emailSender.py $1 >> emailSender_$1.log
+python ../emailHandler/emailSender.py $1 >> emailSender_$1.log
 sleep 30
 done
