4384237aa4329238607f981272050e288e23bf00
David Milet <david.milet@gmail.com>
2017-04-22 20:02:12 +0800

    new try

--- db/create_fresh_carpool_db.sh
+++ db/create_fresh_carpool_db.sh
@@ -9,9 +9,9 @@ fi
 
 psql -U postgres -d postgres -h /tmp < carpool_roles.sql \
 && createdb -U postgres -h /tmp --owner carpool_admin $1 \
-&& psql -h /tmp $1 < carpool_schema_bootstrap.sql \
-&& psql -h /tmp $1 < carpool_schema.sql \
-&& psql -h /tmp $1 < carpool_static_data.sql \
-&& psql -h /tmp $1 < carpool_params_data.sql \
+&& psql -U carpool_admin -h /tmp $1 < carpool_schema_bootstrap.sql \
+&& psql -U carpool_admin -h /tmp $1 < carpool_schema.sql \
+&& psql -U carpool_admin -h /tmp $1 < carpool_static_data.sql \
+&& psql -U carpool_admin -h /tmp $1 < carpool_params_data.sql \
 && ./load_functions.sh $1
 
