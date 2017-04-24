21358f4cdd23db5cf8494a53b0342c23f20d08ee
David Milet <david.milet@gmail.com>
2017-04-24 10:52:26 +0800

    remove cd commands, execute from current dir

--- db/create_fresh_carpool_db.sh
+++ db/create_fresh_carpool_db.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/sh
 
+# should be executed from the directory where the sql files are located
+
 if [[ "X$1" = "X" ]]
 then
 	echo missing DB name
@@ -7,11 +9,11 @@ then
 	exit 1
 fi
 
-su postgres -c "cd /opt/carpool/backend/db && psql < carpool_roles.sql" \ 
+su postgres -c "psql < carpool_roles.sql" \ 
 && su postgres -c "createdb --owner carpool_admin $1" \
-&& su carpool_app -c "cd /opt/carpool/backend/db && psql $1 < carpool_schema_bootstrap.sql" \
-&& su carpool_app -c "cd /opt/carpool/backend/db && psql $1 < carpool_schema.sql" \
-&& su carpool_app -c "cd /opt/carpool/backend/db && psql $1 < carpool_static_data.sql" \
-&& su carpool_app -c "cd /opt/carpool/backend/db && psql $1 < carpool_params_data.sql" \
-&& su carpool_app -c "cd /opt/carpool/backend/db && ./load_functions.sh $1"
+&& su carpool_app -c "psql $1 < carpool_schema_bootstrap.sql" \
+&& su carpool_app -c "psql $1 < carpool_schema.sql" \
+&& su carpool_app -c "psql $1 < carpool_static_data.sql" \
+&& su carpool_app -c "psql $1 < carpool_params_data.sql" \
+&& su carpool_app -c "./load_functions.sh $1"
 
