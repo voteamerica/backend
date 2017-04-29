a803bf313027e79e8d88f9e51eea3dd51e67c762
David Milet <david.milet@gmail.com>
2017-04-29 09:30:44 +0800

    fix column name

--- db/show_submission_counts.sh
+++ db/show_submission_counts.sh
@@ -12,27 +12,27 @@ echo $PGDATABASE
 
 echo "DRIVERS" 
 psql $PGDATABASE <<RPT
-select count(*) , state from carpoolvote.driver group by state;
+select count(*) , status from carpoolvote.driver group by status;
 RPT
 
 echo "RIDERS"
 psql $PGDATABASE <<RPT
-select count(*) , state from carpoolvote.rider group by state;
+select count(*) , status from carpoolvote.rider group by status;
 RPT
 
 echo "MATCHES" 
 psql $PGDATABASE <<RPT
-select count(*) , state from carpoolvote.match group by state;
+select count(*) , status from carpoolvote.match group by status;
 RPT
 
 echo "EMAILS"
 psql $PGDATABASE <<RPT
-select count(*) , state from carpoolvote.outgoing_email group by state;
+select count(*) , status from carpoolvote.outgoing_email group by status;
 RPT
 
 echo "SMS"
 psql $PGDATABASE <<RPT
-select count(*) , state from carpoolvote.outgoing_sms group by state;
+select count(*) , status from carpoolvote.outgoing_sms group by status;
 RPT
 
 
