a4958683d4e19a90c14cd8a6953860451f1c9003
jkbits1 <jkbits1@users.noreply.github.com>
2017-05-05 22:14:43 +0100

    updated script to match latest db creation script

--- docker/pg-auto/create_fresh_carpool_db2.sh
+++ docker/pg-auto/create_fresh_carpool_db2.sh
@@ -7,16 +7,21 @@
 # 	exit 1
 # fi
 
-echo createdb ...
+# this script is run from the dockerfile init folder using the postgres user
+# https://github.com/docker-library/docs/pull/848/files
 
-createdb carpoolvote \
-&& psql carpoolvote < carpool_roles.sql \
-&& psql carpoolvote < carpool_schema_bootstrap.sql \
-&& psql carpoolvote < carpool_schema.sql \
-&& psql carpoolvote < carpool_static_data.sql \
-&& psql carpoolvote < carpool_params_data.sql \
-&& psql carpoolvote < fct_utilities.sql \
-&& psql carpoolvote < fct_outbound_notifications.sql \
-&& psql carpoolvote < fct_user_actions.sql \
-&& psql carpoolvote < fct_matching_engine.sql \
-&& psql carpoolvote < /usr/src/app/backend/docker/pg-auto/alter.sql \
+DB=carpoolvote
+
+echo "createdb $DB"
+
+psql < carpool_roles.sql \
+&& createdb  --owner carpool_admin $DB \
+&& psql $DB < carpool_schema_bootstrap.sql \
+&& psql $DB < carpool_schema.sql \
+&& psql $DB < carpool_static_data.sql \
+&& psql $DB < carpool_params_data.sql \
+&& psql $DB < fct_utilities.sql \
+&& psql $DB < fct_outbound_notifications.sql \
+&& psql $DB < fct_user_actions.sql \
+&& psql $DB < fct_matching_engine.sql \
+&& psql $DB < /usr/src/app/backend/docker/pg-auto/alter.sql \
