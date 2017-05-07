af290417538308be6b35b4ab18023e94539f78a6
David Milet <david.milet@gmail.com>
2017-05-07 21:45:51 +0800

    parameterize configure_carpool_db_for_test.sh

--- db/configure_carpool_db_for_test.sh
+++ db/configure_carpool_db_for_test.sh
@@ -8,19 +8,29 @@
 if [[ "X$1" = "X" ]]
 then
         echo missing DB name
-        echo $0 \<DB name\>
+        echo $0 \<DB name\> \<APIenv\> \<carpool_site_url\>
         exit 1
 fi
 
-IFS='_' read -ra ARRELEMS <<< $1
+if [[ "X$2" = "X" ]]
+then
+        echo missing APIenv
+        echo $0 \<DB name\> \<APIenv\> \<carpool_site_url\>
+        exit 1
+fi
 
-APIENV=${ARRELEMS[1]}
+if [[ "X$3" = "X" ]]
+then
+        echo missing carpool_site_url
+        echo $0 \<DB name\> \<APIenv\> \<carpool_site_url\>
+        exit 1
+fi
 
-echo "Configuring database for environment : " $APIENV
+echo "Configuring database $1 for $2 with site $3"
 
 psql -d $1 <<SQLINPUT
 UPDATE carpoolvote.params SET value='true' WHERE name='outgoing_sms_whitelist.enabled';
-UPDATE carpoolvote.params SET value='http://richardwestenra.com/voteamerica.github.io' WHERE name='site.base.url';
-UPDATE carpoolvote.params SET value='$APIENV' WHERE name='api_environment';
+UPDATE carpoolvote.params SET value='$3' WHERE name='site.base.url';
+UPDATE carpoolvote.params SET value='$2' WHERE name='api_environment';
 SELECT * FROM carpoolvote.params;
 SQLINPUT
