2c35e726b26281d009985bf76d3e339997e8c19c
jkbits1 <jkbits1@users.noreply.github.com>
2017-05-28 02:37:59 +0100

    docker compose scripts

---
+++ docker/specific-machine-test.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+if [[ "X$1" = "X" ]]
+then
+	echo missing machine name
+	echo $0 \<DB name\>
+	# exit 1
+else
+    M=$1
+fi
+
+if [[ "X$2" = "X" ]]; then
+    R=https://github.com/jkbits1/backend
+else 
+    R=$2
+fi
+
+if [[ "X$3" = "X" ]]
+then
+    B=ts-route
+	echo BRANCH_NAME $B
+else
+    B=$3
+fi
+
+echo MACHINE $M
+echo REPO $R
+echo BRANCH_NAME $3
+
+docker-compose -f ./compose/full-stack-test/docker-compose-dev-build-test.yml build --build-arg REPO=$R --build-arg BRANCH_NAME=$B $M
