93f6da8c94b67401f487744939119f817423ab5c
jkbits1 <jkbits1@users.noreply.github.com>
2017-07-05 00:38:08 +0100

    use travis pr env vars

--- docker/specific-machine-test-travis.sh
+++ docker/specific-machine-test-travis.sh
@@ -1,46 +1,96 @@
 #!/bin/bash
 
+# https://docs.travis-ci.com/user/customizing-the-build#Implementing-Complex-Build-Steps
+set -ev
+
 if [[ "X$1" = "X" ]]
 then
 	echo missing machine name
 	echo $0 \<DB name\>
 	# exit 1
 else
-    M=$1
+    MACHINE=$1
 fi
 
 if [[ "X$2" = "XR" ]]
 then
     # R is flag for rebuild, so force a cache bust
-    C=$(date +%s)
+    CACHEBUST=$(date +%s)
 else
     # 1 matches the arg in the dockerfile, this will not bust the cache
-    C=1
-	echo CACHEBUST $C
+    CACHEBUST=1
+	echo CACHEBUST $CACHEBUST
 fi
 
 if [[ "X$3" = "X" ]]; then
-    R=https://github.com/jkbits1/backend
-    echo REPO $R
+    REPO=jkbits1/backend
+    echo REPO $REPO
 else 
-    R=$3
+    REPO=$3
 fi
 
 if [[ "X$4" = "X" ]]
 then
-    B=ts-route
-	echo BRANCH_NAME $B
+    BRANCH=ts-route
+	echo BRANCH $BRANCH
 else
-    B=$4
+    BRANCH=$4
 fi
 
-echo MACHINE $M
-echo CACHEBUST $C
-echo REPO $R
-echo BRANCH_NAME $B
+# handle travis params
+if [[ "X$5" = "X" ]]
+then
+    EVENT=push
+	echo EVENT $EVENT
+else
+    EVENT=$5
+fi
+
+if [[ "X$6" = "X" ]]
+then
+	echo PR_SLUG $PR_SLUG
+else
+    PR_SLUG=$6
+fi
+
+if [[ "X$7" = "X" ]]
+then
+	echo PR_BRANCH $PR_BRANCH
+else
+    PR_BRANCH=$7
+fi
+
+echo MACHINE $MACHINE
+echo CACHEBUST $CACHEBUST
+echo REPO $REPO
+echo BRANCH $BRANCH
+
+echo REPO_SLUG $REPO_SLUG
+echo PR_SLUG $PR_SLUG
+echo EVENT $EVENT
+
+if [[ "X$PR_SLUG" = "X"]]
+    # push event
+    echo PUSH $REPO
+then
+    # pr event
+    $REPO=$PR_SLUG
+
+    echo PR $REPO
+fi
+
+if [[ "X$PR_BRANCH" = "X"]]
+    # push event
+    echo PUSH $BRANCH
+then
+    # pr event
+    $BRANCH=$PR_BRANCH
+
+    echo PR $BRANCH
+fi
 
 pwd
-
 ls
 
-docker-compose -f docker/compose/full-stack-test/docker-compose-test.yml build --build-arg REPO=$R --build-arg BRANCH_NAME=$B --build-arg CACHEBUST=$C $M
+docker-compose -f docker/compose/full-stack-test/docker-compose-test.yml build --build-arg REPO=https://github.com/$REPO --build-arg BRANCH_NAME=$BRANCH --build-arg CACHEBUST=$CACHEBUST $MACHINE
+
