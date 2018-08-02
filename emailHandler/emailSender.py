3759246d662749e14b94867bc999cb8259220ab1
dmilet <david.milet@gmail.com>
2018-08-01 23:14:46 -0400

    remaining files for nginx and email sender

--- emailHandler/emailSender.py
+++ emailHandler/emailSender.py
@@ -3,50 +3,54 @@ import os
 import sys
 import requests
 import psycopg2
-
+import time
 
 ########################################################
-# must pass db name as first argument
+# requires the following environment variables
+# MAILGUNKEY : the mailgun key
+# PGDATABASE : the name of the database
 ########################################################
 
 
-key = os.environ['MAILGUNKEY']
-request_url = 'https://api.mailgun.net/v3/www.carpoolvote.com/messages'
+while True:
+	key = os.environ['MAILGUNKEY']
+	request_url = 'https://api.mailgun.net/v3/www.carpoolvote.com/messages'
 
-try:
-	conn = psycopg2.connect("dbname={0}".format(sys.argv[1]))
-except:
-	print ("I am unable to connect to the database")
-	exit
+	try:
+		conn = psycopg2.connect("dbname={0}".format(os.environ['PGDATABASE']), host='cp_pg_server', user='carpool_app')
+		print ("Connected")
+	except:
+		print ("I am unable to connect to the database")
 
-cur = conn.cursor()
-cur.execute("""SELECT id, recipient, subject, body from carpoolvote.outgoing_email where status='Pending' order by created_ts asc """)
+	cur = conn.cursor()
+	cur.execute("""SELECT id, recipient, subject, body from carpoolvote.outgoing_email where status='Pending' order by created_ts asc """)
 
-rows = cur.fetchall()
+	rows = cur.fetchall()
 
-for row in rows:
-	print (row[1] + ' ' + row[2] + '\n' + row[3] + '\n')
+	for row in rows:
+		print (row[1] + ' ' + row[2] + '\n' + row[3] + '\n')
 
-	request = requests.post(request_url, auth=('api', key), data={
-		'from': 'Carpool Vote <noreply@carpoolvote.com>',
-		'to': row[1],
-		'subject': row[2],
-		'html': row[3]
-		})
+		request = requests.post(request_url, auth=('api', key), data={
+			'from': 'Carpool Vote <noreply@carpoolvote.com>',
+			'to': row[1],
+			'subject': row[2],
+			'html': row[3]
+			})
 
-	print ('Status: {0}'.format(request.status_code))
-	print ('Body:   {0}'.format(request.text))
+		print ('Status: {0}'.format(request.status_code))
+		print ('Body:   {0}'.format(request.text))
 
-	if request.status_code == 200:
-		cur.execute("""UPDATE carpoolvote.outgoing_email
-						SET status='Sent', emission_info = '200 - OK'
-						WHERE id = %s""", (row[0],))
-	else:
-		cur.execute("""UPDATE carpoolvote.outgoing_email
-						SET status='Failed', emission_info = %s
-						WHERE id = %s""", ("{0}: {1}".format(request.status_code,request.text), "{0}".format(row[0]), ))
+		if request.status_code == 200:
+			cur.execute("""UPDATE carpoolvote.outgoing_email
+							SET status='Sent', emission_info = '200 - OK'
+							WHERE id = %s""", (row[0],))
+		else:
+			cur.execute("""UPDATE carpoolvote.outgoing_email
+							SET status='Failed', emission_info = %s
+							WHERE id = %s""", ("{0}: {1}".format(request.status_code,request.text), "{0}".format(row[0]), ))
 
-	conn.commit()
+		conn.commit()
 
-cur.close()
-conn.close()
+	cur.close()
+	conn.close()
+	time.sleep(20);
