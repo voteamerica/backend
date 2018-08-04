2272d18b58f001661603f865167d7fd442837de1
dmilet <david.milet@gmail.com>
2018-08-04 11:48:51 -0400

    refer to default installation path for docker-compose

--- docker/common-sudo-fix.sh
+++ docker/common-sudo-fix.sh
@@ -2,17 +2,17 @@
 
 # pre-requisite : /etc/sudoers.d/docker
 #username        ALL=(ALL)       NOPASSWD: /usr/bin/docker
-#username        ALL=(ALL)       NOPASSWD: /usr/bin/docker-compose
+#username        ALL=(ALL)       NOPASSWD: /usr/local/bin/docker-compose
 
 # https://www.projectatomic.io/blog/2015/08/why-we-dont-let-non-root-users-run-docker-in-centos-fedora-or-rhel/
 
 export DOCKER=/usr/bin/docker
-export DOCKERCOMPOSE=/usr/bin/docker-compose
+export DOCKERCOMPOSE=/usr/local/bin/docker-compose
 
 # check if docker commands need sudo or not
 docker ps >/dev/null 2>&1
 [[ $? != 0 ]] && {
     export DOCKER="sudo /usr/bin/docker"
-    export DOCKERCOMPOSE="sudo /usr/bin/docker-compose"
+    export DOCKERCOMPOSE="sudo /usr/local/bin/docker-compose"
 }
 
