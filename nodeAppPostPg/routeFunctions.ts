d6992182c78aa2b36f17e898809255c044e30ac8
jkbits1 <jkbits1@users.noreply.github.com>
2018-08-08 22:53:00 +0100

    user insert query returns values

--- nodeAppPostPg/routeFunctions.ts
+++ nodeAppPostPg/routeFunctions.ts
@@ -70,7 +70,7 @@ async function addUserInternal (req: any, reply: any) {
 
   req.log();
 
-  const insertPlusValues = queryFn => ()=> queryFn() + ' ("email", "username", "password", "admin") ' + " values ($1, $2, $3, $4)";
+  const insertPlusValues = queryFn => ()=> queryFn() + ' ("email", "username", "password", "admin") ' + " values ($1, $2, $3, $4) returning " + '"UUID", email, username, admin';
 
   const dbData = await postgresQueries.dbInsertDataInternal(payload, rfPool, insertPlusValues(dbQueries.dbAddUserQueryString), getInsertUserPayloadAsArray, req, reply, results);
 
