14b00b457b8fef019eca2a0cda34971e67c99ec7
David Milet <david.milet@gmail.com>
2018-08-25 16:12:18 -0400

    fix db creation on Vagrant

--- db/create_fresh_carpool_db_docker.sh
+++ db/create_fresh_carpool_db_docker.sh
@@ -1,5 +1,3 @@
-# needs to be executed as postgres user
-
 # optional environment variables
 # For docker, use :
 # CARPOOL_DATABASE_NAME=LIVE
@@ -10,15 +8,14 @@ CARPOOL_SOURCE_FILES=${CARPOOL_SOURCE_FILES:-.}
 
 cd $CARPOOL_SOURCE_FILES
 
-psql < carpool_roles.sql \
-&& createdb --owner carpool_admin $CARPOOL_DATABASE_NAME \
-&& psql $CARPOOL_DATABASE_NAME < carpool_schema_bootstrap.sql \
-&& psql $CARPOOL_DATABASE_NAME < carpool_schema.sql \
-&& psql $CARPOOL_DATABASE_NAME < carpool_static_data.sql \
-&& psql $CARPOOL_DATABASE_NAME < carpool_params_data.sql \
-&& psql $CARPOOL_DATABASE_NAME < fct_utilities.sql \
-&& psql $CARPOOL_DATABASE_NAME < carpool_views.sql \
-&& psql $CARPOOL_DATABASE_NAME < fct_outbound_notifications.sql \
-&& psql $CARPOOL_DATABASE_NAME < fct_user_actions.sql \
-&& psql $CARPOOL_DATABASE_NAME < fct_matching_engine.sql
-
+psql -d postgres -U postgres < carpool_roles.sql  \
+&& su - postgres -c "createdb --owner carpool_admin $CARPOOL_DATABASE_NAME" \
+&& psql -U postgres -d $CARPOOL_DATABASE_NAME < carpool_schema_bootstrap.sql \
+&& psql -U postgres -d $CARPOOL_DATABASE_NAME < carpool_schema.sql \
+&& psql -U postgres -d $CARPOOL_DATABASE_NAME < carpool_static_data.sql \
+&& psql -U postgres -d $CARPOOL_DATABASE_NAME < carpool_params_data.sql \
+&& psql -U postgres -d $CARPOOL_DATABASE_NAME < fct_utilities.sql \
+&& psql -U postgres -d $CARPOOL_DATABASE_NAME < carpool_views.sql \
+&& psql -U postgres -d $CARPOOL_DATABASE_NAME < fct_outbound_notifications.sql \
+&& psql -U postgres -d $CARPOOL_DATABASE_NAME < fct_user_actions.sql \
+&& psql -U postgres -d $CARPOOL_DATABASE_NAME < fct_matching_engine.sql
