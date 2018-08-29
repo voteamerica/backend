dd260df1781ffbabfd923879cc97fdd1518f3b1d
jkbits1 <jkbits1@users.noreply.github.com>
2018-08-29 02:41:53 +0100

    convert nodeapp and db to use tb_user.is_admin field, dev env has test user

--- docker/pg-auto/test-user.sql
+++ docker/pg-auto/test-user.sql
@@ -1,2 +1,2 @@
-INSERT INTO carpoolvote.tb_user (email, username, password, admin)
-    VALUES ('e@t.com', 'test', 'abc', false);
+INSERT INTO carpoolvote.tb_user (email, username, password, is_admin)
+    VALUES ('e@t.com', 'test', '$2b$10$HDeBIi2iPeMznaa5.wR4tuY6M9SxM4KI8/1XTEvP69Ai1yRSFwC1.', false);
