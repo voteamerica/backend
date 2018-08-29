bd22d851c829cc5a9805212e9edc30321b84cd62
jkbits1 <jkbits1@users.noreply.github.com>
2018-08-29 22:45:31 +0100

    create full tests script from pause_cancel2 script

---
+++ nodeAppPostPg/testing/nightwatch/tests/full-tests/full-tests.js
@@ -0,0 +1,25 @@
+var common = require('../../common/common.js');
+
+module.exports = {
+  'full-match-pause': function (client) {
+
+    common.matchRiderDriver(client)
+      .driverCancelMatch()
+      
+      .nextDate()
+      .addDriver()
+      .viewDriverSelfService()
+      .pauseDriverSelfService()
+      .cancelDriverSelfService()
+      .addRider()
+      .viewRiderSelfService()
+      .cancelRiderSelfService()      
+
+      .nextDate()
+      .matchRiderDriver()
+      .viewRiderSelfService()
+      .viewRiderMatch()
+      .riderCancelMatch()
+      .finish();
+  }
+};
