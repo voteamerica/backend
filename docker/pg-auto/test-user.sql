9b5c01626ebbcf738c1d8cd6a7ac4a8acc663e66
jkbits1 <jkbits1@users.noreply.github.com>
2018-09-23 00:49:41 +0100

    dev env default users now have an org uuid

--- docker/pg-auto/test-user.sql
+++ docker/pg-auto/test-user.sql
@@ -1,4 +1,4 @@
-INSERT INTO carpoolvote.tb_user (email, username, password, is_admin)
-    VALUES ('admin@test.com', 'admin', '$2b$10$HDeBIi2iPeMznaa5.wR4tuY6M9SxM4KI8/1XTEvP69Ai1yRSFwC1.', true);
-INSERT INTO carpoolvote.tb_user (email, username, password, is_admin)
-    VALUES ('notadmin@test.com', 'notadmin', '$2b$10$HDeBIi2iPeMznaa5.wR4tuY6M9SxM4KI8/1XTEvP69Ai1yRSFwC1.', false);
+INSERT INTO carpoolvote.tb_user (email, username, password, is_admin, "UUID_organization")
+    VALUES ('admin@test.com', 'admin', '$2b$10$HDeBIi2iPeMznaa5.wR4tuY6M9SxM4KI8/1XTEvP69Ai1yRSFwC1.', true, '8b8b907d-cb69-4f2e-bf1a-65681ff2f35e');
+INSERT INTO carpoolvote.tb_user (email, username, password, is_admin, "UUID_organization")
+    VALUES ('notadmin@test.com', 'notadmin', '$2b$10$HDeBIi2iPeMznaa5.wR4tuY6M9SxM4KI8/1XTEvP69Ai1yRSFwC1.', false, '8b8b907d-cb69-4f2e-bf1a-65681ff2f35e');
