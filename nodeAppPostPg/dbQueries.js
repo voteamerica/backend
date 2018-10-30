2d4fca64c0dafd3f377f2763ef1f87e216955551
jkbits1 <jkbits1@users.noreply.github.com>
2018-10-30 03:41:18 +0000

    record of some test queries

--- nodeAppPostPg/dbQueries.js
+++ nodeAppPostPg/dbQueries.js
@@ -299,4 +299,230 @@ function dbGetUserOrganizationQueryString(username) {
 //     carpoolvote.driver.status_info, "DriverPreferredContact", "DriverWillTakeCare",
 //     carpoolvote.driver.uuid_organization FROM carpoolvote.match inner join carpoolvote.rider on(carpoolvote.match.uuid_rider = carpoolvote.rider."UUID") inner join carpoolvote.driver on carpoolvote.match.uuid_driver = carpoolvote.driver."UUID"
 // where rider."TotalPartySize" < driver."SeatCount"
+// seats query
+// SELECT uuid_driver, "DriverFirstName", "DriverLastName", "SeatCount", sum("TotalPartySize"), sum("TotalPartySize")> "SeatCount"
+// --count(*)
+// --, "RiderFirstName", "RiderLastName", match.status
+//   FROM carpoolvote.match
+//   INNER JOIN carpoolvote.rider ON uuid_rider = carpoolvote.rider."UUID"
+//   INNER JOIN carpoolvote.driver ON uuid_driver = carpoolvote.driver."UUID"
+//   where match.status = 'MatchConfirmed'
+//   group by uuid_driver, "DriverFirstName", "DriverLastName", "SeatCount"
+// SELECT uuid_driver, "DriverFirstName", "DriverLastName", "SeatCount", count(*), sum("TotalPartySize"), sum("TotalPartySize")> "SeatCount"
+// --count(*)
+// --, "RiderFirstName", "RiderLastName", match.status
+//   FROM carpoolvote.match
+//   INNER JOIN carpoolvote.rider ON uuid_rider = carpoolvote.rider."UUID"
+//   INNER JOIN carpoolvote.driver ON uuid_driver = carpoolvote.driver."UUID"
+//   where match.status = 'MatchConfirmed'
+//   group by uuid_driver, "DriverFirstName", "DriverLastName", "SeatCount"
+// SELECT uuid_driver, "DriverFirstName", "DriverLastName", "SeatCount", count(*), sum("TotalPartySize") as "TotalRiders", sum("TotalPartySize")> "SeatCount" , div(sum("TotalPartySize"), "SeatCount")
+// + mod(sum("TotalPartySize"), "SeatCount") * 1
+// --count(*)
+// --, "RiderFirstName", "RiderLastName", match.status
+//   FROM carpoolvote.match
+//   INNER JOIN carpoolvote.rider ON uuid_rider = carpoolvote.rider."UUID"
+//   INNER JOIN carpoolvote.driver ON uuid_driver = carpoolvote.driver."UUID"
+//   where match.status = 'MatchConfirmed'
+//   group by uuid_driver, "DriverFirstName", "DriverLastName", "SeatCount"
+// SELECT uuid_driver, "DriverFirstName", "DriverLastName", "SeatCount", count(*) as "MatchCount", sum("TotalPartySize") as "TotalRiders", sum("TotalPartySize") > "SeatCount" as "Overflow", "SeatCount" - sum("TotalPartySize") as "SeatsAvailable", div(sum("TotalPartySize"), "SeatCount") as "approxTripCount"
+// --, "RiderFirstName", "RiderLastName", match.status
+// FROM carpoolvote.match
+// INNER JOIN carpoolvote.rider ON uuid_rider = carpoolvote.rider."UUID"
+// INNER JOIN carpoolvote.driver ON uuid_driver = carpoolvote.driver."UUID"
+// where match.status = 'MatchConfirmed'
+// group by uuid_driver, "DriverFirstName", "DriverLastName", "SeatCount"
+// select * from carpoolvote.driver
+// inner join
+//   (SELECT uuid_driver, "DriverFirstName", "DriverLastName", "SeatCount", count(*) as "MatchCount", sum("TotalPartySize") as "TotalRiders", sum("TotalPartySize") > "SeatCount" as "Overflow", "SeatCount" - sum("TotalPartySize") as "SeatsAvailable", div(sum("TotalPartySize"), "SeatCount") as "approxTripCount"
+// --, "RiderFirstName", "RiderLastName", match.status
+//   FROM carpoolvote.match
+//   INNER JOIN carpoolvote.rider ON uuid_rider = carpoolvote.rider."UUID"
+//   INNER JOIN carpoolvote.driver ON uuid_driver = carpoolvote.driver."UUID"
+//   where match.status = 'MatchConfirmed'
+//   group by uuid_driver, "DriverFirstName", "DriverLastName", "SeatCount"
+//   ) matchInfo on "UUID" = uuid_driver
+// SELECT carpoolvote.driver."UUID", "IPAddress", "DriverCollectionZIP", "DriverCollectionRadius"
+// "AvailableDriveTimesLocal", "DriverCanLoadRiderWithWheelchair",
+//   "SeatCount", "DriverLicenseNumber", "DriverFirstName", "DriverLastName",
+//   "DriverEmail", "DriverPhone", "DrivingOnBehalfOfOrganization",
+//   "DrivingOBOOrganizationName", "RidersCanSeeDriverDetails", "DriverWillNotTalkPolitics",
+//   "ReadyToMatch", "PleaseStayInTouch", status, created_ts, last_updated_ts,
+//   status_info, "DriverPreferredContact", "DriverWillTakeCare",
+//   uuid_organization, city, state, full_state, timezone,
+//   "MatchCount", "TotalRiders", "Overflow", "SeatsAvailable", "approxTripCount", "modTC"
+// FROM carpoolvote.driver
+// inner join
+//   (SELECT uuid_driver, count(*) as "MatchCount", sum("TotalPartySize") as "TotalRiders", sum("TotalPartySize") > "SeatCount" as "Overflow", "SeatCount" - sum("TotalPartySize") as "SeatsAvailable", div(sum("TotalPartySize"), "SeatCount") as "approxTripCount",
+//   (case when(mod(sum("TotalPartySize"), "SeatCount") = 0) then 0 else 1 end) as "modTC"
+// --, "RiderFirstName", "RiderLastName", match.status
+// FROM carpoolvote.match
+// INNER JOIN carpoolvote.rider ON uuid_rider = carpoolvote.rider."UUID"
+// INNER JOIN carpoolvote.driver ON uuid_driver = carpoolvote.driver."UUID"
+// where match.status = 'MatchConfirmed'
+// group by uuid_driver, "DriverFirstName", "DriverLastName", "SeatCount"
+//   ) matchInfo on "UUID" = uuid_driver
+// INNER JOIN carpoolvote.organization ON(("DrivingOnBehalfOfOrganization" is TRUE and "DrivingOBOOrganizationName" = "OrganizationName") or("DrivingOnBehalfOfOrganization" is FALSE and 'None' = "OrganizationName"))
+// INNER JOIN carpoolvote.zip_codes ON "DriverCollectionZIP" = zip
+// SELECT carpoolvote.driver."UUID", "IPAddress", "DriverCollectionZIP", "DriverCollectionRadius"
+// "AvailableDriveTimesLocal", "DriverCanLoadRiderWithWheelchair",
+//   "SeatCount", "DriverLicenseNumber", "DriverFirstName", "DriverLastName",
+//   "DriverEmail", "DriverPhone", "DrivingOnBehalfOfOrganization",
+//   "DrivingOBOOrganizationName", "RidersCanSeeDriverDetails", "DriverWillNotTalkPolitics",
+//   "ReadyToMatch", "PleaseStayInTouch", status, created_ts, last_updated_ts,
+//   status_info, "DriverPreferredContact", "DriverWillTakeCare",
+//   uuid_organization, city, state, full_state, timezone,
+//   "MatchCount", "TotalRiders", "Overflow", "SeatsAvailable", "minimumTripCount"
+// FROM carpoolvote.driver
+// inner join
+//   (SELECT uuid_driver, count(*) as "MatchCount", sum("TotalPartySize") as "TotalRiders", sum("TotalPartySize") > "SeatCount" as "Overflow", "SeatCount" - sum("TotalPartySize") as "SeatsAvailable", (div(sum("TotalPartySize"), "SeatCount") + (case when(mod(sum("TotalPartySize"), "SeatCount") = 0) then 0 else 1 end)) as "minimumTripCount"
+// --, "RiderFirstName", "RiderLastName", match.status
+// FROM carpoolvote.match
+// INNER JOIN carpoolvote.rider ON uuid_rider = carpoolvote.rider."UUID"
+// INNER JOIN carpoolvote.driver ON uuid_driver = carpoolvote.driver."UUID"
+// where match.status = 'MatchConfirmed'
+// group by uuid_driver, "DriverFirstName", "DriverLastName", "SeatCount"
+//   ) matchInfo on "UUID" = uuid_driver
+// INNER JOIN carpoolvote.organization ON(("DrivingOnBehalfOfOrganization" is TRUE and "DrivingOBOOrganizationName" = "OrganizationName") or("DrivingOnBehalfOfOrganization" is FALSE and 'None' = "OrganizationName"))
+// INNER JOIN carpoolvote.zip_codes ON "DriverCollectionZIP" = zip
+// (SELECT carpoolvote.driver."UUID", "IPAddress", "DriverCollectionZIP", "DriverCollectionRadius"
+// "AvailableDriveTimesLocal", "DriverCanLoadRiderWithWheelchair",
+//   "SeatCount", "DriverLicenseNumber", "DriverFirstName", "DriverLastName",
+//   "DriverEmail", "DriverPhone", "DrivingOnBehalfOfOrganization",
+//   "DrivingOBOOrganizationName", "RidersCanSeeDriverDetails", "DriverWillNotTalkPolitics",
+//   "ReadyToMatch", "PleaseStayInTouch", status, created_ts, last_updated_ts,
+//   status_info, "DriverPreferredContact", "DriverWillTakeCare",
+//   uuid_organization, city, state, full_state, timezone,
+//   "MatchCount", "TotalRiders", "Overflow", "SeatsAvailable", "minimumTripCount"
+// FROM carpoolvote.driver
+// inner join
+//   (SELECT uuid_driver, count(*) as "MatchCount", sum("TotalPartySize") as "TotalRiders", sum("TotalPartySize") > "SeatCount" as "Overflow", "SeatCount" - sum("TotalPartySize") as "SeatsAvailable", (div(sum("TotalPartySize"), "SeatCount") + (case when(mod(sum("TotalPartySize"), "SeatCount") = 0) then 0 else 1 end)) as "minimumTripCount"
+// --, "RiderFirstName", "RiderLastName", match.status
+// FROM carpoolvote.match
+// INNER JOIN carpoolvote.rider ON uuid_rider = carpoolvote.rider."UUID"
+// INNER JOIN carpoolvote.driver ON uuid_driver = carpoolvote.driver."UUID"
+// where match.status = 'MatchConfirmed'
+// group by uuid_driver, "DriverFirstName", "DriverLastName", "SeatCount"
+//   ) matchInfo on "UUID" = uuid_driver
+// INNER JOIN carpoolvote.organization ON(("DrivingOnBehalfOfOrganization" is TRUE and "DrivingOBOOrganizationName" = "OrganizationName") or("DrivingOnBehalfOfOrganization" is FALSE and 'None' = "OrganizationName"))
+// INNER JOIN carpoolvote.zip_codes ON "DriverCollectionZIP" = zip)
+// union
+//   (SELECT carpoolvote.driver."UUID", "IPAddress", "DriverCollectionZIP", "DriverCollectionRadius"
+//        "AvailableDriveTimesLocal", "DriverCanLoadRiderWithWheelchair",
+//   "SeatCount", "DriverLicenseNumber", "DriverFirstName", "DriverLastName",
+//   "DriverEmail", "DriverPhone", "DrivingOnBehalfOfOrganization",
+//   "DrivingOBOOrganizationName", "RidersCanSeeDriverDetails", "DriverWillNotTalkPolitics",
+//   "ReadyToMatch", "PleaseStayInTouch", status, created_ts, last_updated_ts,
+//   status_info, "DriverPreferredContact", "DriverWillTakeCare",
+//   uuid_organization, city, state, full_state, timezone,
+//   0 as "MatchCount", 0 as "TotalRiders", false as "Overflow", 0 as "SeatsAvailable", 0 as "minimumTripCount"
+//   FROM carpoolvote.driver
+// inner join
+//     (SELECT uuid_driver
+//   FROM carpoolvote.match
+//   INNER JOIN carpoolvote.rider ON uuid_rider = carpoolvote.rider."UUID"
+//   INNER JOIN carpoolvote.driver ON uuid_driver = carpoolvote.driver."UUID"
+//   where match.status != 'MatchConfirmed'
+//   group by uuid_driver, "DriverFirstName", "DriverLastName", "SeatCount"
+//     ) matchInfo on "UUID" = uuid_driver
+//   INNER JOIN carpoolvote.organization ON(("DrivingOnBehalfOfOrganization" is TRUE and "DrivingOBOOrganizationName" = "OrganizationName") or("DrivingOnBehalfOfOrganization" is FALSE and 'None' = "OrganizationName"))
+// INNER JOIN carpoolvote.zip_codes ON "DriverCollectionZIP" = zip
+// )
+// (SELECT carpoolvote.driver."UUID", "IPAddress", "DriverCollectionZIP", "DriverCollectionRadius"
+// "AvailableDriveTimesLocal", "DriverCanLoadRiderWithWheelchair",
+//   "SeatCount", "DriverLicenseNumber", "DriverFirstName", "DriverLastName",
+//   "DriverEmail", "DriverPhone", "DrivingOnBehalfOfOrganization",
+//   "DrivingOBOOrganizationName", "RidersCanSeeDriverDetails", "DriverWillNotTalkPolitics",
+//   "ReadyToMatch", "PleaseStayInTouch", status, created_ts, last_updated_ts,
+//   status_info, "DriverPreferredContact", "DriverWillTakeCare",
+//   uuid_organization, city, state, full_state, timezone,
+//   "MatchCount", "TotalRiders", "Overflow", "SeatsAvailable", "minimumTripCount"
+// FROM carpoolvote.driver
+// inner join
+//   (SELECT uuid_driver, count(*) as "MatchCount", sum("TotalPartySize") as "TotalRiders", sum("TotalPartySize") > "SeatCount" as "Overflow", "SeatCount" - sum("TotalPartySize") as "SeatsAvailable", (div(sum("TotalPartySize"), "SeatCount") + (case when(mod(sum("TotalPartySize"), "SeatCount") = 0) then 0 else 1 end)) as "minimumTripCount"
+// --, "RiderFirstName", "RiderLastName", match.status
+// FROM carpoolvote.match
+// INNER JOIN carpoolvote.rider ON uuid_rider = carpoolvote.rider."UUID"
+// INNER JOIN carpoolvote.driver ON uuid_driver = carpoolvote.driver."UUID"
+// where match.status = 'MatchConfirmed'
+// group by uuid_driver, "DriverFirstName", "DriverLastName", "SeatCount"
+//   ) matchInfo on "UUID" = uuid_driver
+// INNER JOIN carpoolvote.organization ON(("DrivingOnBehalfOfOrganization" is TRUE and "DrivingOBOOrganizationName" = "OrganizationName") or("DrivingOnBehalfOfOrganization" is FALSE and 'None' = "OrganizationName"))
+// INNER JOIN carpoolvote.zip_codes ON "DriverCollectionZIP" = zip)
+// union
+//   (SELECT carpoolvote.driver."UUID", "IPAddress", "DriverCollectionZIP", "DriverCollectionRadius"
+//        "AvailableDriveTimesLocal", "DriverCanLoadRiderWithWheelchair",
+//   "SeatCount", "DriverLicenseNumber", "DriverFirstName", "DriverLastName",
+//   "DriverEmail", "DriverPhone", "DrivingOnBehalfOfOrganization",
+//   "DrivingOBOOrganizationName", "RidersCanSeeDriverDetails", "DriverWillNotTalkPolitics",
+//   "ReadyToMatch", "PleaseStayInTouch", status, created_ts, last_updated_ts,
+//   status_info, "DriverPreferredContact", "DriverWillTakeCare",
+//   uuid_organization, city, state, full_state, timezone,
+//   0 as "MatchCount", 0 as "TotalRiders", false as "Overflow", 0 as "SeatsAvailable", 0 as "minimumTripCount"
+//   FROM carpoolvote.driver
+// inner join
+//     (SELECT uuid_driver
+//   FROM carpoolvote.match
+//   INNER JOIN carpoolvote.rider ON uuid_rider = carpoolvote.rider."UUID"
+//   INNER JOIN carpoolvote.driver ON uuid_driver = carpoolvote.driver."UUID"
+//   where match.status != 'MatchConfirmed'
+//   group by uuid_driver, "DriverFirstName", "DriverLastName", "SeatCount"
+//     ) matchInfo on "UUID" = uuid_driver
+//   INNER JOIN carpoolvote.organization ON(("DrivingOnBehalfOfOrganization" is TRUE and "DrivingOBOOrganizationName" = "OrganizationName") or("DrivingOnBehalfOfOrganization" is FALSE and 'None' = "OrganizationName"))
+// INNER JOIN carpoolvote.zip_codes ON "DriverCollectionZIP" = zip
+// where uuid_driver not in (SELECT uuid_driver FROM carpoolvote.match
+// INNER JOIN carpoolvote.driver ON uuid_driver = carpoolvote.driver."UUID"
+// where match.status = 'MatchConfirmed'
+// )
+// )
+// select * from((SELECT carpoolvote.driver."UUID", "IPAddress", "DriverCollectionZIP", "DriverCollectionRadius"
+//        "AvailableDriveTimesLocal", "DriverCanLoadRiderWithWheelchair",
+//   "SeatCount", "DriverLicenseNumber", "DriverFirstName", "DriverLastName",
+//   "DriverEmail", "DriverPhone", "DrivingOnBehalfOfOrganization",
+//   "DrivingOBOOrganizationName", "RidersCanSeeDriverDetails", "DriverWillNotTalkPolitics",
+//   "ReadyToMatch", "PleaseStayInTouch", status, created_ts, last_updated_ts,
+//   status_info, "DriverPreferredContact", "DriverWillTakeCare",
+//   uuid_organization, city, state, full_state, timezone,
+//   "MatchCount", "TotalRiders", "Overflow", "SeatsAvailable", "minimumTripCount"
+//   FROM carpoolvote.driver
+// inner join
+//     (SELECT uuid_driver, count(*) as "MatchCount", sum("TotalPartySize") as "TotalRiders", sum("TotalPartySize") > "SeatCount" as "Overflow", "SeatCount" - sum("TotalPartySize") as "SeatsAvailable", (div(sum("TotalPartySize"), "SeatCount") + (case when(mod(sum("TotalPartySize"), "SeatCount") = 0) then 0 else 1 end)) as "minimumTripCount"
+// --, "RiderFirstName", "RiderLastName", match.status
+// FROM carpoolvote.match
+// INNER JOIN carpoolvote.rider ON uuid_rider = carpoolvote.rider."UUID"
+// INNER JOIN carpoolvote.driver ON uuid_driver = carpoolvote.driver."UUID"
+// where match.status = 'MatchConfirmed'
+// group by uuid_driver, "DriverFirstName", "DriverLastName", "SeatCount"
+//   ) matchInfo on "UUID" = uuid_driver
+// INNER JOIN carpoolvote.organization ON(("DrivingOnBehalfOfOrganization" is TRUE and "DrivingOBOOrganizationName" = "OrganizationName") or("DrivingOnBehalfOfOrganization" is FALSE and 'None' = "OrganizationName"))
+// INNER JOIN carpoolvote.zip_codes ON "DriverCollectionZIP" = zip)
+// union
+//   (SELECT carpoolvote.driver."UUID", "IPAddress", "DriverCollectionZIP", "DriverCollectionRadius"
+//        "AvailableDriveTimesLocal", "DriverCanLoadRiderWithWheelchair",
+//   "SeatCount", "DriverLicenseNumber", "DriverFirstName", "DriverLastName",
+//   "DriverEmail", "DriverPhone", "DrivingOnBehalfOfOrganization",
+//   "DrivingOBOOrganizationName", "RidersCanSeeDriverDetails", "DriverWillNotTalkPolitics",
+//   "ReadyToMatch", "PleaseStayInTouch", status, created_ts, last_updated_ts,
+//   status_info, "DriverPreferredContact", "DriverWillTakeCare",
+//   uuid_organization, city, state, full_state, timezone,
+//   0 as "MatchCount", 0 as "TotalRiders", false as "Overflow", 0 as "SeatsAvailable", 0 as "minimumTripCount"
+//   FROM carpoolvote.driver
+//   INNER JOIN carpoolvote.organization ON(("DrivingOnBehalfOfOrganization" is TRUE and "DrivingOBOOrganizationName" = "OrganizationName") or("DrivingOnBehalfOfOrganization" is FALSE and 'None' = "OrganizationName"))
+// INNER JOIN carpoolvote.zip_codes ON "DriverCollectionZIP" = zip
+// where driver."UUID" not in (SELECT uuid_driver as "UUID" FROM carpoolvote.match
+// INNER JOIN carpoolvote.driver ON uuid_driver = carpoolvote.driver."UUID"
+// where match.status = 'MatchConfirmed'
+// )
+// )) drivers
+// INNER JOIN carpoolvote.tb_user ON carpoolvote.tb_user."UUID_organization" = uuid_organization
+// ,
+// "IPAddress", "DriverCollectionZIP", driver."DriverCollectionRadius"
+// "AvailableDriveTimesLocal", "DriverCanLoadRiderWithWheelchair",
+//   "SeatCount", "DriverLicenseNumber", "DriverFirstName", "DriverLastName",
+//   "DriverEmail", "DriverPhone", "DrivingOnBehalfOfOrganization",
+//   "DrivingOBOOrganizationName", "RidersCanSeeDriverDetails", "DriverWillNotTalkPolitics",
+//   "ReadyToMatch", "PleaseStayInTouch", status, created_ts, last_updated_ts,
+//   status_info, "DriverPreferredContact", "DriverWillTakeCare",
+//   uuid_organization, city, state, full_state, timezone,
+//   "MatchCount", "TotalRiders", "Overflow", "SeatsAvailable", "minimumTripCount"
 //# sourceMappingURL=dbQueries.js.map
